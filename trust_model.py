553a847e361eb9399b474e48c89a93748d76dacb
rcosta349 <rafael.costa2109@gmail.com>
2025-06-12 22:18:13 +0100

    improving exit_security function with @fjsilveira

--- trust_model.py
+++ trust_model.py
@@ -7,7 +7,6 @@ def guard_security(client_country, guards, alliances):
         guard_country = guard_relay.get("country")
         trust = DEFAULT_TRUST
 
-        # TODO compare destination/guard
         for alliance in alliances:
             countries = alliance["countries"]
 
@@ -16,7 +15,7 @@ def guard_security(client_country, guards, alliances):
                 break
 
         if client_country == guard_country:
-            trust *= 0.2
+            trust *= 0.5
 
         guard_relay["trust"] = trust
 
@@ -25,12 +24,18 @@ def exit_security(client_country, dest_country, guard, exits, alliances):
     for exit_relay in exits:
         exit_country = exit_relay.get("country")
         trust = DEFAULT_TRUST
+        has_trust = False
 
-        # TODO compare exit/guard and exit/destination
         for alliance in alliances:
             countries = alliance["countries"]
             if client_country in countries and exit_country in countries:
+                has_trust = True
                 trust = alliance["trust"]
+            if guard["country"] in countries and exit_country in countries:
+                if has_trust:
+                    trust *= alliance["trust"]
+                else:
+                    trust = alliance["trust"]
                 break
 
         if is_in_family(guard["fingerprint"], exit_relay):
@@ -40,12 +45,12 @@ def exit_security(client_country, dest_country, guard, exits, alliances):
             trust *= 0.2
 
         if exit_country == guard.get("country"):
-            trust *= 0.4
+            trust *= 0.5
 
         if exit_country == dest_country:
             trust *= 0.5
 
         if exit_country == client_country:
-            trust *= 0.4
+            trust *= 0.5
 
         exit_relay["trust"] = trust
